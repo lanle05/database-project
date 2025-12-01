4b74ce46af42492be8db676907b8a4480d164d81
Omobolanle <omobolanle.shaibu@gmail.com>
2025-12-01 22:25:35 +0100

    All Done! part 4 abeg nau

--- server/db.js
+++ server/db.js
@@ -1,6 +1,7 @@
 import mysql from "mysql2";
 
 const pool = mysql.createPool({
+  database: process.env.DB_NAME || "student_identity_db",
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "Leavemealoneabeg@123!", // Fallback to local password
